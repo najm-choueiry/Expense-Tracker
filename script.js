9de5b5d618505a267a833c581f91f16caf58df16
najm-choueiry <nachoueiry@outlook.com>
2023-07-19 07:56:45 +0300

    fixing errors

--- script.js
+++ script.js
@@ -1,30 +1,35 @@
-function expenseTracker(text) {
+function expenseTracker(expense) {
     return
     `
     <td>${expense}</td>
-    <td>${location}</td>
-    <td>${amount}</td>
+    <td class="remove">&#128465</td>
+
     `
 }
 
 
 function addExpense() {
-    const expense = $('#expense').val()
-    const location = $('#location').val()
-    const amount = $('#amount').val()
-    const output = $('$output')
+    const expense = $('#expense')
+    // const location = $('#location').val()
+    // const amount = $('#amount').val()
+    const output = $('#output')
 
+    if (expense.val().trim() === "") return ;
+
+    output.append(expenseTracker(expense))
     const expense_item = $(expenseTracker(expense))
-    const location_item = $(expenseTracker(location))
-    const amount_item = $(expenseTracker(amount))
+    // const location_item = $(expenseTracker(location))
+    // const amount_item = $(expenseTracker(amount))
 
 
-    if (expense.val().trim() ==="" || amount.val().trim() ==="" || amount.val().trim()==="") return ; 
+    // if (expense.val().trim() ==="" || amount.val().trim() ==="" || amount.val().trim()==="") return ; 
 
-    output.append(expenseTracker)
-    expense.val("")
-    location.val("")
-    amount.val("")
+    
+
+    // output.append(expenseTracker)
+    // expense.val("")
+    // location.val("")
+    // amount.val("")
 
 }
 
@@ -32,11 +37,11 @@ $(document).ready(function () {
     const add = $("#submit");
     // add item
     add.click(addExpense);
-  
+
     $("#amount").keyup(function (event) {
-      if (event.keyCode === 13) {
-        addExpense()
-      }
-    }
-    )
-})
+        if (event.keyCode === 13) {
+            addExpense();
+        }
+    });
+});
+
